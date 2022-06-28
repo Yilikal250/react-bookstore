511fdb8c272ee0492bd3fb69a54c19f77677ec20
Yilikal250 <teshomeyilikal250@gmail.com>
2022-06-28 11:01:04 +0300

    linter corrected

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,7 +1,7 @@
 import { configureStore, combineReducers } from 'react-redux';
 
 import reducerBook from './book/book';
-import reducerCategories from './categories/categories';
+import reducerCategories from './catagories/catagories';
 
 const reducer = combineReducers({
   books: reducerBook,
